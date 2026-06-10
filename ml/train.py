2f00b5e27217a9c50b6bccd5cd2b28795bae0daf
TomHarcus <tomharcus17@gmail.com>
2026-06-10 16:52:51 +0100

    training script

--- ml/train.py
+++ ml/train.py
@@ -1,20 +1,39 @@
-import pandas as pd
-from transformers import pipeline
+from transformers import AutoTokenizer
+from datasets import load_dataset, DatasetDict
 
-DATA_FILE = "ml/data/sales_convo_labelled.csv"
+dataset = load_dataset("TomHarcus/sales-behaviour-classifier-dataset", split="train")
 
-def get_counts(data):
-    df = pd.read_csv(data)
+shuffled_dataset = dataset.shuffle()
 
-    column_counts = df["label"].value_counts()
+train_test = shuffled_dataset.train_test_split(test_size=0.3)
 
-    return column_counts
+train_test_valid = train_test["test"].train_test_split(test_size=0.5)
 
-classifier = pipeline(
-    task="text-classification",
-    model="distilbert-base-uncased-finetuned-sst-2-english",
-    device=0
-)
+datasets = DatasetDict({
+    "train": train_test["train"],
+    "test": train_test_valid["test"],
+    "valid": train_test_valid["train"]
+})
 
-result = classifier("I dont have an opinion for hugging face transformers")
-print(result)
+tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
+
+def tokenize(sample):
+    return tokenizer(sample["context_turns"], sample["response"], truncation=True)
+
+
+
+def label_to_int(sample):
+    label_map = {
+    "WC": 0,
+    "OH": 1,
+    "FD": 2,
+    "AN": 3
+    }
+
+    return {"labels": label_map[sample["label"]]} 
+
+tokenized_dataset = datasets.map(tokenize, batched=True).map(label_to_int)
+
+tokenized_dataset = tokenized_dataset.remove_columns(["label", "context_turns", "response"])
+
+print(tokenized_dataset)
